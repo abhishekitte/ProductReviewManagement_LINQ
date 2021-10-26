c7a05e4e516cdb97d4fae17997da537763ee0865
ABHI <abhishekitte11@gmail.com>
2021-10-27 00:46:46 +0530

    [ABHISHEK] Added a code to create list of 25 product review

---
+++ ProductReviewManagment_LINQ/ProductReview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductReviewManagement_LINQ
+{
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
